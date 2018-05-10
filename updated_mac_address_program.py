81544bd26da10b101b3a18caa762e7f20c502591
raju-007 <37836029+raju-007@users.noreply.github.com>
2018-05-10 16:36:47 +0530

    Update updated_mac_address_program.py

--- updated_mac_address_program.py
+++ updated_mac_address_program.py
@@ -13,6 +13,6 @@ with open("mac.txt") as fo:
             ki.append(match.group(0))
     print("mac address present in text file is=")
     print(ki)
-    for j in ki:
-        f1.write(j+"\n")
+    for j, i in enumerate(ki,1):
+        f1.write(str(j)+"="+str(i)+"\n")
 
